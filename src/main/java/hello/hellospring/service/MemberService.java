e7028a4ec10e40896e1f78080d8a7051e6b573b2
eomjunseong <hi_man159@naver.com>
2022-05-29 21:20:21 +0900

    0529

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,9 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -34,8 +37,8 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(s->{
-                    throw new IllegalStateException("이미 존재하는 회원임");
-                });
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
     }
 
 }
